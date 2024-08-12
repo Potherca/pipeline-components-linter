65fbb8141072fec6224d81057514e9427980702a
Ben Peachey <ben@muze.nl>
2024-08-12 20:46:59 +0200

    Add first working draft.

--- cmd/plc-lint/plc-lint.go
+++ cmd/plc-lint/plc-lint.go
@@ -1,8 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"internal/check"
+	"internal/message"
+	"os"
+	"slices"
+)
+
+func listFiles(path string) (fileNames []string, err error) {
+	files, err := os.ReadDir(path)
+
+	if err == nil {
+		for _, file := range files {
+			name := file.Name()
+			if file.IsDir() {
+				name += "/"
+			}
+			fileNames = append(fileNames, name)
+		}
+
+	}
+
+	return fileNames, err
+}
 
 func main() {
-	// output to console
-	fmt.Println("Hello, World!")
+	path := "."
+
+	fileNames, err := listFiles(path)
+
+	var fileCodes = make(map[string]string)
+
+	fileCodes["app/"] = "PLC4001"
+	fileCodes[".github/"] = "PLC4002"
+	fileCodes[".gitignore"] = "PLC5001"
+	fileCodes[".gitlab-ci.yml"] = "PLC5002"
+	fileCodes[".mdlrc"] = "PLC5003"
+	fileCodes[".yamllint"] = "PLC5004"
+	fileCodes["action.yml"] = "PLC5005"
+	fileCodes["Dockerfile"] = "PLC5006"
+	fileCodes["LICENSE"] = "PLC5007"
+	fileCodes["README.md"] = "PLC5008"
+	fileCodes["renovate.json"] = "PLC5009"
+
+	if err == nil {
+		requiredFiles := [11]string{
+			".github/",
+			"app/",
+			".gitignore",
+			".gitlab-ci.yml",
+			".mdlrc",
+			".yamllint",
+			"action.yml",
+			"Dockerfile",
+			"LICENSE",
+			"README.md",
+			"renovate.json",
+		}
+
+		for _, file := range requiredFiles {
+			fileType := "file"
+
+			if file[len(file)-1:] == "/" {
+				fileType = "directory"
+			}
+
+			status := check.Fail
+			if slices.Contains(fileNames, file) {
+				status = check.Pass
+			}
+
+			checkMessage := fmt.Sprintf("The repository MUST contain a %s %s", file, fileType)
+			code := fileCodes[file]
+
+			var messageMarker = message.Marker{
+				Pass: "✅",
+				Fail: "❌",
+			}
+
+			fmt.Print(message.CreateMessage(status, code, checkMessage, messageMarker))
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module gitlab.com/pipeline-components/pipeline-component-linter
 
 go 1.22.6
+
+require internal/check v0.1.0
+replace internal/check => ./internal/check
+
+require internal/message v0.1.0
+replace internal/message => ./internal/message
