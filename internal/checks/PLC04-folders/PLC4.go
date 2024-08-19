559bae87c269e277ab118f9fed55ecc31acb9a7c
Ben Peachey <ben@muze.nl>
2024-08-19 22:45:46 +0200

    Change PLC4 to use FolderExists assert.

--- internal/checks/PLC04-folders/PLC4.go
+++ internal/checks/PLC04-folders/PLC4.go
@@ -1,39 +1,15 @@
 package checks
 
 import (
-	"fmt"
-	"internal/check"
+	"internal/asserts"
 	"internal/message"
-	"maps"
-	"slices"
 )
 
 func PLC4(files map[string]string) []message.Message {
-	var messages []message.Message
-
 	var fileCodes = make(map[string]string)
 
 	fileCodes["app/"] = "PLC4001"
 	fileCodes[".github/"] = "PLC4002"
 
-	requiredFiles := [2]string{
-		".github/",
-		"app/",
-	}
-
-	fileNames := slices.Collect(maps.Keys(files))
-
-	for _, file := range requiredFiles {
-		status := check.Fail
-		if slices.Contains(fileNames, file) {
-			status = check.Pass
-		}
-
-		checkMessage := fmt.Sprintf("The repository MUST contain a %s directory", file)
-		code := fileCodes[file]
-
-		messages = append(messages, message.CreateMessage(status, code, checkMessage))
-	}
-
-	return messages
+	return asserts.FolderExists(files, fileCodes)
 }
