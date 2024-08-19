d86b5c1826c0af9e36f57a8eb9e503f87c9e47ee
Ben Peachey <ben@muze.nl>
2024-08-19 11:38:48 +0200

    Add logic to accept path as CLI param.

--- cmd/plc-lint/plc-lint.go
+++ cmd/plc-lint/plc-lint.go
@@ -5,19 +5,64 @@ import (
 	"internal/check"
 	plc4 "internal/checks/PLC04-folders"
 	plc5 "internal/checks/PLC05-files"
+	"internal/exitcodes"
 	"internal/message"
 	"os"
+	"path/filepath"
 )
 
-func listFiles(path string) (fileNames []string, err error) {
+type commandError struct {
+	code    int
+	message string
+}
+
+func getPath(projectPath string) (string, commandError) {
+	userError := commandError{
+		code:    0,
+		message: "",
+	}
+
+	projectPath, _ = filepath.Abs(projectPath)
+	fileInfo, err := os.Stat(projectPath)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			userError = commandError{
+				code:    exitcodes.CouldNotFindDirectory,
+				message: fmt.Sprintf("provided path '%s' does not exist", projectPath),
+			}
+		} else {
+			userError = commandError{
+				code:    exitcodes.UnknownErrorOccurred,
+				message: fmt.Sprintf("could not stat path '%s': %v", projectPath, err),
+			}
+		}
+	} else {
+		if !fileInfo.IsDir() {
+			userError = commandError{
+				code:    exitcodes.InvalidParameter,
+				message: fmt.Sprintf("provided path '%s' is not a directory", projectPath),
+			}
+		}
+	}
+
+	return projectPath, userError
+}
+
+func listFiles(path string) ([]string, error) {
+	var fileNames []string
+
 	files, err := os.ReadDir(path)
 
-	if err == nil {
+	if err != nil {
+		err = fmt.Errorf("could not read files from '%s': %w", path, err)
+	} else {
 		for _, file := range files {
 			name := file.Name()
 			if file.IsDir() {
 				name += "/"
 			}
+
 			fileNames = append(fileNames, name)
 		}
 	}
@@ -26,18 +71,30 @@ func listFiles(path string) (fileNames []string, err error) {
 }
 
 func main() {
-	path := "."
+	projectPath := "."
 
-	var messages []message.Message
+	if len(os.Args) > 1 {
+		projectPath = os.Args[1]
+	}
+
+	projectPath, pathError := getPath(projectPath)
+
+	if pathError.code != 0 {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", pathError.message)
+		os.Exit(pathError.code)
+	}
 
 	var messageMarker = message.Marker{
 		Pass: "✅",
 		Fail: "❌",
 	}
 
-	fileNames, err := listFiles(path)
+	fileNames, err := listFiles(projectPath)
 
-	if err == nil {
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 
 		messages = append(messages, plc4.PLC4(fileNames)...)
 		messages = append(messages, plc5.PLC5(fileNames)...)
