c8459444c8642ef02180eebc49be8e66663605b3
Ben Peachey <ben@muze.nl>
2024-09-20 18:46:38 +0200

    Add more checks for PLC13 README.md file.

--- internal/checks/PLC13-README.md-file/PLC13.go
+++ internal/checks/PLC13-README.md-file/PLC13.go
@@ -8,7 +8,9 @@ import (
 	"github.com/gomarkdown/markdown/parser"
 	"internal/check"
 	"internal/message"
+	"net/http"
 	"reflect"
+	"regexp"
 	"strings"
 )
 
@@ -25,10 +27,15 @@ func listCodes() map[string]string {
 		"PLC13007": "The 'Contributing' section in the `README.md` file MUST be identical to their counterparts in the `README.md` file in the skeleton repository",
 		"PLC13008": "⁉ The 'Examples' section in the `README.md` file MUST be auto-generated from a separate example file in the repository",
 		"PLC13009": "The 'Authors & contributors' section in the `README.md` file MUST state the author who initially set up the repository",
-		"PLC13010": "The 'Authors & contributors' section in the `README.md` file MUST link to the contributor's page",
-		"PLC13011": "The 'License' section in the `README.md` file MUST state the author who initially set up the repository",
-		"PLC13012": "The 'License' section in the `README.md` file MUST state the license type as MIT",
-		"PLC13013": "The 'License' section in the `README.md` file MUST link to the license file in the repository",
+		"PLC13010": "The 'Authors & contributors' section in the `README.md` file MAY contain a link for the initial author",
+		"PLC13011": "The link for the initial author in the 'Authors & contributors' section in the `README.md` file, if present, MUST resolve",
+		"PLC13012": "The 'Authors & contributors' section in the `README.md` file MUST link to the contributor's page",
+		"PLC13013": "The contributor's page link in the 'Authors & contributors' section in the `README.md` file MUST resolve",
+		"PLC13014": "The 'License' section in the `README.md` file MUST attribute Robbert Müller as creator",
+		"PLC13015": "The attribution in the 'License' section in the `README.md` file MUST resolve to https://gitlab.com/mjrider",
+		"PLC13016": "The 'License' section in the `README.md` file MUST state the license type as MIT",
+		"PLC13017": "The 'License' section in the `README.md` file MUST link to the license file in the repository",
+		"PLC13018": "The license link in the 'License' section in the `README.md` file MUST resolve",
 	}
 }
 
@@ -155,7 +162,21 @@ func getSections(document ast.Node) map[string]string {
 	return sections
 }
 
-func PLC13(files map[string]string, repo map[string]string) []message.Message {
+func urlResolves(url string) bool {
+	resolves := false
+
+	response, err := http.Get(url)
+
+	if err == nil {
+		if response.StatusCode >= 200 && response.StatusCode <= 399 {
+			resolves = true
+		}
+	}
+
+	return resolves
+}
+
+func PLC13(componentName string, files map[string]string, repo map[string]string) []message.Message {
 	var (
 		messages []message.Message
 		ok       bool
@@ -173,10 +194,8 @@ func PLC13(files map[string]string, repo map[string]string) []message.Message {
 			codes["PLC13002"] = fmt.Sprintf("The required `%s` file is missing from the skeleton repository", targetFile)
 			status["PLC13002"] = check.Error
 		} else {
-			for code := range codes {
-				if code != "PLC13001" && code != "PLC13008" {
-					status[code] = check.Fail
-				}
+			for _, code := range []string{"PLC13002", "PLC13003", "PLC13004"} {
+				status[code] = check.Fail
 			}
 
 			subjectParser := parser.NewWithExtensions(parser.CommonExtensions)
@@ -205,6 +224,126 @@ func PLC13(files map[string]string, repo map[string]string) []message.Message {
 			if reflect.DeepEqual(subjectHeadings, skeletonHeadings) {
 				status["PLC13004"] = check.Pass
 			} // else { @TODO: Show the differences between the headings }
+
+			for checkCode, sectionName := range map[string]string{
+				"PLC13005": "Versioning",
+				"PLC13006": "Support",
+				"PLC13007": "Contributing",
+			} {
+				if _, ok := subjectSections[sectionName]; ok {
+					if subjectSections[sectionName] == skeletonSections[sectionName] {
+						status[checkCode] = check.Pass
+					} else {
+						status[checkCode] = check.Fail
+					}
+				}
+			}
+
+			linkPattern := regexp.MustCompile(`\[(?P<Subject>[^]]+)\]\((?P<URL>[^)]+)\)`)
+
+			if _, ok := subjectSections["Authors & contributors"]; ok {
+				for _, code := range []string{"PLC13009", "PLC13012", "PLC13013"} {
+					status[code] = check.Fail
+				}
+
+				authorLines := strings.Split(subjectSections["Authors & contributors"], "\n")
+				for _, line := range authorLines {
+
+					if strings.Contains(line, "The original setup of this repository is by ") {
+						split := strings.Split(line, "by ")
+
+						if len(split) > 1 && split[1] != "" {
+							// @TODO: Use git history to validate user? If so, the check wording MUST be changed to reflect this!
+							status["PLC13009"] = check.Pass
+
+							if linkPattern.MatchString(split[1]) {
+								status["PLC13010"] = check.Pass
+								status["PLC13011"] = check.Fail
+
+								matches := linkPattern.FindStringSubmatch(split[1])
+								url := matches[linkPattern.SubexpIndex("URL")]
+
+								if urlResolves(url) {
+									status["PLC13011"] = check.Pass
+								}
+							}
+						}
+					} else if strings.Contains(line, "contributor's page") {
+						matches := linkPattern.FindStringSubmatch(line)
+						url := matches[linkPattern.SubexpIndex("URL")]
+
+						if linkPattern.MatchString(line) {
+							pattern := `https://gitlab.com/pipeline-components/(?:[^/]+/)?(?P<Component>[^/]+)/-/graphs/main`
+							contributorsLinkPattern := regexp.MustCompile(pattern)
+
+							if contributorsLinkPattern.MatchString(line) {
+								status["PLC13012"] = check.Pass
+								status["PLC13013"] = check.Fail
+
+								if urlResolves(url) {
+									status["PLC13013"] = check.Pass
+								}
+							}
+						}
+					}
+				}
+			}
+
+			if _, ok := subjectSections["License"]; ok {
+				for _, code := range []string{"PLC13014", "PLC13015", "PLC13016", "PLC13017", "PLC13018"} {
+					status[code] = check.Fail
+				}
+
+				licenseLines := strings.Split(subjectSections["License"], "\n")
+				for _, line := range licenseLines {
+					if strings.Contains(line, "Created by [Robbert Müller]") {
+						status["PLC13014"] = check.Pass
+						status["PLC13015"] = check.Fail
+
+						split := strings.Split(line, "by ")
+
+						if len(split) > 1 && split[1] != "" {
+							if linkPattern.MatchString(split[1]) {
+								matches := linkPattern.FindStringSubmatch(split[1])
+								url := matches[linkPattern.SubexpIndex("URL")]
+
+								if urlResolves(url) {
+									status["PLC13015"] = check.Pass
+								}
+							}
+						}
+					}
+					if strings.Contains(line, "licensed under a ") {
+						split := strings.Split(line, "under a ")
+
+						if len(split) > 1 && split[1] != "" {
+							if linkPattern.MatchString(split[1]) {
+								matches := linkPattern.FindStringSubmatch(split[1])
+								url := matches[linkPattern.SubexpIndex("URL")]
+								subject := matches[linkPattern.SubexpIndex("Subject")]
+
+								if subject == "MIT license" || subject == "MIT License" {
+									status["PLC13016"] = check.Pass
+								}
+
+								if url == "./LICENSE" {
+									status["PLC13017"] = check.Pass
+
+									url = fmt.Sprintf(
+										"https://gitlab.com/pipeline-components/%s/-/blob/HEAD/%s",
+										componentName,
+										url,
+									)
+
+									if urlResolves(url) {
+										status["PLC13018"] = check.Pass
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
